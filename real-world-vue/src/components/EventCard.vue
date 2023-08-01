e3236b530e24f657ed2415465ec404cf9981c53c
Gianluca Starke <gianlucastarke@gmail.com>
2023-08-01 07:03:02 -0300

    Adicionado link para o detalhe do evento (estático)

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
 
 defineProps({
   event: {
@@ -10,10 +11,12 @@ defineProps({
 </script>
 
 <template>
-  <div class="event-card">
-    <h2>{{ event.title }}</h2>
-    <span>@{{ event.time }} on {{ event.date }}</span>
-  </div>
+  <RouterLink to="event/123">
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </RouterLink>
 </template>
 
 <style scoped>
@@ -23,9 +26,13 @@ defineProps({
   cursor: pointer;
   border: 1px solid #39495c;
   margin-bottom: 18px;
+  color: rgb(6, 77, 0);
 }
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+a {
+  text-decoration: none;
 }
 </style>
